bcb42dd599992bfbb1804cc236d4498a4bf6c008
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-10-28 22:28:45 -0500

    I've got movement!

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
 int main()
@@ -6,6 +7,12 @@ int main()
     window.setFramerateLimit(144);
 
     sf::CircleShape shape(50.f);
+
+    // Physical quantities
+    sf::Vector2f a(0.f, 9.8f);
+    sf::Vector2f v(0.f, 0.f);
+    sf::Vector2f x(0.f, 0.f);
+    float dt = 0.01f;
 
     // set the shape color to green
     shape.setFillColor(sf::Color(100, 250, 50));
@@ -20,9 +27,17 @@ int main()
             }
         }
 
+        // Update using leapfrog algorithm
+        v += a * dt / 2.f;
+        x += v * dt;
+        v += a * dt / 2.f;
+
+        shape.setPosition(x);
+
         // clear the window with black color
         window.clear(sf::Color::Black);
 
+        // draw frame
         window.draw(shape);
 
         // end the current frame

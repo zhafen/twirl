ec38781b2cb008988beee5df839a343b9202fd6f
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-10-31 23:18:58 -0500

    Added gravitational force + scalings.

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,32 @@
-#include <iostream>
-#include <cmath>
 #include <SFML/Graphics.hpp>
+#include <cmath>
+#include <iostream>
 
 class Particle {
-public:
-    Particle(sf::Vector2f x, sf::Vector2f v, float r) : x(x), v(v), shape(r) {
+   public:
+    Particle(sf::Vector2f r, sf::Vector2f v, float R) : r(r), v(v), shape(R) {
         // Set the origin as the center of the particle
-        shape.setOrigin(r, r);
-        shape.setPosition(x);
+        shape.setOrigin(R, R);
+        shape.setPosition(r);
     }
 
     void update(sf::Vector2f a, float dt) {
         // Update using leapfrog algorithm
         v += a * dt / 2.f;
-        x += v * dt;
+        r += v * dt;
         v += a * dt / 2.f;
+        shape.setPosition(r);
     }
+    sf::Vector2f r;
+    sf::Vector2f v;
 
     sf::CircleShape getShape() const { return shape; }
 
-private:
-    sf::Vector2f x;
-    sf::Vector2f v;
+   private:
     sf::CircleShape shape;
 };
 
-int main()
-{
+int main() {
     // Window settings
     auto window = sf::RenderWindow({1920u, 1080u}, "twirl");
     int fps = 144;
@@ -36,40 +36,48 @@ int main()
     float dt = 1.f / fps;
     sf::Vector2u window_size = window.getSize();
     // d will be our length scale, and we set it to a multiple of window size
+    // We also introduce velocity, time, and acceleration scales
     float d(0.01f * window_size.x);
+    float t(0.1f);
+    float s(d / t);
+    float g(s / t);
 
     // Create and activate a view
     sf::View view(sf::Vector2f(0, 0), sf::Vector2f(window_size.x, window_size.y));
-    window.setView(view);
 
-    Particle p(sf::Vector2f(0.f, 0.f), sf::Vector2f(0.f, 0.f), d);
+    Particle p(sf::Vector2f(0.f, 20.f * d), sf::Vector2f(0.f, -s), d);
 
-    // Reference rectangle
-    sf::RectangleShape rectangle(sf::Vector2f(d, d));
-    rectangle.setFillColor(sf::Color(125, 125, 125));
+    Particle p2(sf::Vector2f(5.f * d, 5.f * d), sf::Vector2f(0.f, 0.f), d);
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event(); window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
+    sf::RectangleShape rectangle(sf::Vector2f(10.f * d, 10.f * d));
+    rectangle.setPosition(0.f, 0.f);
+    rectangle.setFillColor(sf::Color(128, 128, 128));
+
+    while (window.isOpen()) {
+        for (auto event = sf::Event(); window.pollEvent(event);) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
 
-        // // Gravitational force
-        // r = x2 - x;
-        // float r2 = pow(r.x, 2.f) + pow(r.y, 2.f);
-        // a = r / (pow(r2, 1.5f) + 10.f);
+        // Gravitational force
+        sf::Vector2f r = p2.r - p.r;
+        float r2 = pow(r.x, 2.f) + pow(r.y, 2.f);
+        sf::Vector2f a =
+            5.f * g * (r / pow(r2 + pow(0.1f * d, 2.f), 1.5f)) * d * d;
 
         // clear the window with black color
         window.clear(sf::Color::Black);
-        p.update(sf::Vector2f(d, d), 100);
+        p.update(a, dt);
 
         // draw frame
         window.draw(rectangle);
         window.draw(p.getShape());
+        window.draw(p2.getShape());
+
+        // Set the view
+        view.setCenter(p.r);
+        window.setView(view);
 
         // end the current frame
         window.display();

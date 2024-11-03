dc1556dd244123139665630ab48356410378fd8e
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-11-03 10:36:14 -0600

    Enemies move.

--- src/main.cpp
+++ src/main.cpp
@@ -22,12 +22,13 @@ int main() {
     std::random_device rd;
     std::mt19937 gen(rd());  // Standard random number generator
     std::uniform_real_distribution<float> dist(-10.f * cfg.L, 10.f * cfg.L);
-    std::vector<sf::CircleShape> enemy_circles(10);
-    for (int i = 0; i < enemy_circles.size(); ++i) {
-        sf::CircleShape cir_i(cfg.L);
-        cir_i.setFillColor(sf::Color::Red);
-        cir_i.setPosition(dist(gen), dist(gen));
-        enemy_circles[i] = cir_i;
+    std::vector<Particle> enemies;
+    int n_enemies = 10;
+    enemies.reserve(n_enemies);
+    for (int i = 0; i < n_enemies; ++i) {
+        enemies.emplace_back(sf::Vector2f(dist(gen), dist(gen)), sf::Vector2f(0.f, 0.f),
+                         cfg.L, cfg);
+        enemies[i].setFillColor(sf::Color::Red);
     }
 
     // Announcement text
@@ -60,13 +61,20 @@ int main() {
 
         // Collision detection
         bool is_colliding = false;
-        for (int i = 0; i < enemy_circles.size(); ++i) {
-            bool is_colliding_i =
-                p.body_particle.getGlobalBounds().intersects(enemy_circles[i].getGlobalBounds());
+        for (int i = 0; i < enemies.size(); ++i) {
+            bool is_colliding_i = p.body_particle.getGlobalBounds().intersects(
+                enemies[i].getGlobalBounds());
             is_colliding = is_colliding | is_colliding_i;
         }
 
         // Update game state
+        for (int i = 0; i < enemies.size(); ++i) {
+            sf::Vector2f r_et = p.body_particle.r - enemies[i].getPosition();
+            float r2 = powf(r_et.x, 2.f) + powf(r_et.y, 2.f);
+            sf::Vector2f a = 5.f * cfg.A * (r_et / powf(r2 + powf(cfg.L, 2.f), 1.5f)) *
+                             cfg.L * cfg.L;
+            enemies[i].updateState(a, cfg.dt);
+        }
         p.updateState(is_colliding);
 
         // Set the view
@@ -80,8 +88,8 @@ int main() {
         for (int i = 0; i < bkgrd_circles.size(); ++i) {
             window.draw(bkgrd_circles[i]);
         }
-        for (int i = 0; i < enemy_circles.size(); ++i) {
-            window.draw(enemy_circles[i]);
+        for (int i = 0; i < enemies.size(); ++i) {
+            window.draw(enemies[i]);
         }
         if (is_colliding) {
             window.draw(announcement);

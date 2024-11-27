e090974ee89cb40b50134ca9a391aee4804136f4
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-11-26 23:01:20 -0600

    Added getEntityMap to the game loop.

--- src/game.cpp
+++ src/game.cpp
@@ -92,6 +92,7 @@ void Game::update() {
     // Try spawning, deleting, and ordering entities
     entity_system.deleteEntities(registry);
     entity_system.spawnEntities(registry);
+    entity_system.getEntityMap(registry);
     entity_system.resolveEntityPairs(registry);
     entity_system.orderEntities(registry);
 

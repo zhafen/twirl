ecb24684de14b47b86ab07fadcd1387317c46f57
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-11-29 16:42:19 -0600

    Added json loading capability for boilerplate builder components.

--- src/python/boilerplate_builder.py
+++ src/python/boilerplate_builder.py
@@ -1,16 +1,71 @@
+from typing import Tuple
 
 
 class BoilerplateBuilder:
     """Create boilerplate code for different the twirl C++ source code."""
 
-    def get_struct_str(self, name: str, fields: dict = {}) -> str:
+    def get_struct_str(self, name: str, members: dict = {}) -> str:
 
-        if len(fields) == 0:
-            return f"struct {name} {{}};"
+        if len(members) == 0:
+            return f"struct {name}Comp {{}};"
 
-        struct_str = f"struct {name} {{\n"
-        for field, field_type in fields.items():
-            struct_str += f"    {field_type} {field};\n"
-        struct_str += "};"
+        # Add the members
+        struct_body_str = ""
+        json_body_str = ""
+        instance_str = name.lower() + "_c"
+        for member_name, member_args in members.items():
+
+            # Formatting for each member
+            if isinstance(member_args, str):
+                member_args = [member_args]
+            member_str, member_json_str = self.get_member_str(member_name, *member_args)
+
+            # Add the member to the existing struct and json strings
+            struct_body_str += "    " + member_str
+            if member_json_str is not None:
+                json_body_str += f"    {instance_str}.{member_json_str}"
+
+        # Add the struct and json strings to the final output
+        struct_str = (
+            "struct {name}Comp {{\n" +
+            struct_body_str +
+            ";}\n"
+        )
+
+        # If there's json to load, add that too.
+        if json_body_str != "":
+            struct_str += (
+                f"inline void from_json(const json& j, {name}& {instance_str}) {{\n" +
+                json_body_str +
+                "}\n"
+            )
+
 
         return struct_str
+
+    def get_member_str(
+        self,
+        member_name: str,
+        member_type: str,
+        json: bool = False,
+        default: str = None,
+    ) -> Tuple[str]:
+
+        # Get the member string for the struct
+        member_str = f"{member_type} {member_name}"
+        if default is not None:
+            member_str += " = " + str(default)
+        member_str += ";\n"
+
+        if not json:
+            return member_str, None
+
+        # Get the from_json string
+        member_json_str = f"{member_name} = j."
+        if default is not None:
+            member_json_str += f'value("{member_name}", {default})'
+        else:
+            member_json_str += f'at("{member_name}").get<{member_type}>()'
+        member_json_str += ";\n"
+
+        return member_str, member_json_str
--- tests/test_python/test_boilerplate_builder.py
+++ tests/test_python/test_boilerplate_builder.py
@@ -1,31 +1,60 @@
 import unittest
 from src.python.boilerplate_builder import BoilerplateBuilder
 
+
 class TestBoilerplateBuilder(unittest.TestCase):
 
     def setUp(self):
-        self.bb = BoilerplateBuilder()
+        self.builder = BoilerplateBuilder()
 
-    def test_get_struct_str_empty_fields(self):
+    def test_get_struct_str_empty_members(self):
         name = "MyStruct"
-        fields = {}
+        members = {}
         expected_output = "struct MyStruct {};"
-        self.assertEqual(self.bb.get_struct_str(name, fields), expected_output)
+        self.assertEqual(self.builder.get_struct_str(name, members), expected_output)
 
-    def test_get_struct_str_single_field(self):
+    def test_get_struct_str_single_member(self):
         name = "MyStruct"
-        fields = {"field1": "int"}
-        expected_output = "struct MyStruct {\n    int field1;\n};"
-        self.assertEqual(self.bb.get_struct_str(name, fields), expected_output)
+        members = {"member1": "int"}
+        expected_output = "struct MyStruct {\n    int member1;\n};"
+        self.assertEqual(self.builder.get_struct_str(name, members), expected_output)
 
-    def test_get_struct_str_multiple_fields(self):
+    def test_get_struct_str_multiple_members(self):
         name = "MyStruct"
-        fields = {"field1": "int", "field2": "float", "field3": "std::string"}
+        members = {"member1": "int", "member2": "float", "member3": "std::string"}
         expected_output = (
             "struct MyStruct {\n"
-            "    int field1;\n"
-            "    float field2;\n"
-            "    std::string field3;\n"
+            "    int member1;\n"
+            "    float member2;\n"
+            "    std::string member3;\n"
             "};"
         )
-        self.assertEqual(self.bb.get_struct_str(name, fields), expected_output)
+        self.assertEqual(self.builder.get_struct_str(name, members), expected_output)
+
+    def test_get_struct_w_from_json(self):
+
+        expected_output = (
+            "struct SceneTriggerComp {\n"
+            "    std::string scene_name;\n"
+            "    entt::entity scene_entity = entt::null;\n"
+            "    int n_triggers = 0;\n"
+            "};\n"
+            "inline void from_json(const json& j, SceneTriggerComp& scenetrigger_c) {\n"
+            '    scenetrigger_c.scene_name = j.at("scene_name").get<std::string>();\n'
+            '    scenetrigger_c.n_triggers = j.value("n_triggers", 0);\n'
+            "}\n"
+        )
+
+        output = self.builder.get_struct_str(
+            "SceneTriggerComp",
+            {
+                "scene_name": [
+                    "std::string",
+                    True,
+                ],
+                "scene_entity": ["entt::entity", False, "entt::null"],
+                "n_triggers": ["int", True, 0],
+            },
+        )
+
+        assert expected_output == output

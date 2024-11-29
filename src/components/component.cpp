b3e24ed26455435032e9849f9f4b9abe1e814f20
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-11-29 12:18:22 -0600

    Removed code that won't work.

--- src/components/component.cpp
+++ src/components/component.cpp
@@ -5,25 +5,12 @@
 
 namespace twirl {
 
-// Define a type map
-std::unordered_map<std::string, std::type_index> compTypeMap = {
-    {"EnemyComp", std::type_index(typeid(EnemyComp))},
-    {"DebugComp", std::type_index(typeid(DebugComp))},
-};
-
 namespace comp {
 
 entt::entity getEntityFromCompStr(entt::registry& registry,
                                   const std::string& comp_str) {
 
-    // Define a type map
-    std::unordered_map<std::string, std::type_index> typeMap = {
-        {"EnemyComp", std::type_index(typeid(EnemyComp))},
-        {"DebugComp", std::type_index(typeid(DebugComp))},
-    };
-
-    std::type_index comp_type = typeMap.at(comp_str);
-    auto rview = registry.view<comp_type>();
+    // auto rview = registry.view<comp_type>();
 
     return entt::null;
 }

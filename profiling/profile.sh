266f7bb2a7208bf45858d679854082473acffbed
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-11-28 22:54:45 -0600

    Added a script for profiling!

---
+++ profiling/profile.sh
@@ -0,0 +1,6 @@
+xctrace record \
+  --template 'CPU Profiler' \
+  --no-prompt \
+  --output /Users/zhafen/repos/twirl/profiling/default.trace \
+  --target-stdout - \
+  --launch /Users/zhafen/repos/twirl/build/bin/twirl

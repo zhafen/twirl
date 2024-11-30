aa5ef75c69f3c99a2e31a93ed67157129f563c72
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-11-29 18:30:02 -0600

    Tested using scaling for default values.

--- include/components/component.h
+++ include/components/component.h
@@ -62,18 +62,15 @@ struct DeleteComp {};
 
 struct PhysicsComp {
     float mass = 1.0f;
-    sf::Vector2f pos = {0.0f, 0.0f};
-    sf::Vector2f vel = {0.0f, 0.0f};
-    sf::Vector2f force = {0.0f, 0.0f};
+    sf::Vector2f pos = sf::Vector2f(0.0f, 0.0f) * cfg.L;
+    sf::Vector2f vel = sf::Vector2f(0.0f, 0.0f) * cfg.V;
+    sf::Vector2f force = sf::Vector2f(0.0f, 0.0f) * cfg.A;
     bool collided = false;
 };
-// Have to define this without a macro because of the vector types
-inline void from_json(const json& j, PhysicsComp& pc) {
-    pc.mass = j.value("mass", 1.0f);
-    pc.pos = j.value("pos", sf::Vector2f(0.0f, 0.0f)) * cfg.L;
-    pc.vel = j.value("vel", sf::Vector2f(0.0f, 0.0f)) * cfg.V;
-    pc.force = j.value("force", sf::Vector2f(0.0f, 0.0f)) * cfg.A;
-    pc.collided = j.value("collided", false);
+inline void from_json(const json& j, PhysicsComp& physicscomp) {
+    physicscomp.mass = j.value("mass", 1.0f);
+    physicscomp.pos = j.value("pos", sf::Vector2f(0.0f, 0.0f)) * cfg.L;
+    physicscomp.vel = j.value("vel", sf::Vector2f(0.0f, 0.0f)) * cfg.V;
 }
 
 struct DragForceComp {

5ff33e7ebab13671af61c84f6fb13c8f8a910687
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-12-12 21:08:43 -0600

    Relocated tests scenes, since I'm setting up some basic tests.

---
+++ tests/test_scenario.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+#include <entt/entity/fwd.hpp>
+#include <entt/entity/helper.hpp>
+#include <nlohmann/json.hpp>
+
+#include "config.h"
+#include "components/components.h"
+#include "game.h"
+
+using namespace twirl;
+
+TEST(ScenarioTest, TestScene) {
+
+    // Initialize the game, hidden in the constructor
+    Game game("../../tests/test_data/test_scene.json");
+    game.run();
+}

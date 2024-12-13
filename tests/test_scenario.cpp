c049dae65fb92e3a377575fe0e70613d2d29eea3
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-12-12 21:30:17 -0600

    Test scenarios actually work.

--- tests/test_scenario.cpp
+++ tests/test_scenario.cpp
@@ -13,6 +13,6 @@ using namespace twirl;
 TEST(ScenarioTest, TestScene) {
 
     // Initialize the game, hidden in the constructor
-    Game game("../../tests/test_data/test_scene.json");
+    Game game("../../tests/test_data/test_scenes/main_test_scene.json");
     game.run();
 }

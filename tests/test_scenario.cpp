016875eab4fafed7734fda960b0d9e740d37bc39
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-12-14 15:42:10 -0600

    Cleaned up TestMenuScene

--- tests/test_scenario.cpp
+++ tests/test_scenario.cpp
@@ -21,51 +21,8 @@ TEST(ScenarioTest, TestScene) {
 
 TEST(ScenarioTest, TestMenuScene) {
 
-    // DEBUG
-    // create the window
-    // sf::RenderWindow window(sf::VideoMode(1600, 900), "My window");
-
-    // // run the program as long as the window is open
-    // while (window.isOpen())
-    // {
-    //     // check all the window's events that were triggered since the last iteration of the loop
-    //     sf::Event event;
-    //     while (window.pollEvent(event))
-    //     {
-    //         // "close requested" event: we close the window
-    //         if (event.type == sf::Event::Closed)
-    //             window.close();
-    //     }
-
-    //     // clear the window with black color
-    //     window.clear(sf::Color::Black);
-
-    //     sf::Font font;
-    //     font.loadFromFile("../../assets/arial.ttf");
-    //     sf::Text text;
-    //     text.setFont(font);
-    //     text.setString("Hello, world!");
-    //     text.setCharacterSize(24);
-    //     text.setFillColor(sf::Color::Red);
-
-    //     // draw everything here...
-    //     window.draw(text);
-
-    //     // end the current frame
-    //     window.display();
-    // }
-
     // Initialize the game, hidden in the constructor
     Game game("../../tests/test_data/test_scenes/test_menu.json");
-
-    // DEBUG
-    // center text
-    // auto& registry = game.getRegistry();
-    // auto& text_c = registry.get<TextComp>(registry.view<TextComp>().front());
-    // sf::FloatRect textRect = text_c.text.getLocalBounds();
-    // text_c.text.setOrigin(textRect.left + textRect.width / 2.0f,
-    //           textRect.top + textRect.height / 2.0f);
-
-    game.max_time = 5.0f;
+    game.max_time = MAX_TIME;
     game.run();
 }

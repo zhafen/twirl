677ae766f803e8f0edf4dd6a689b697eab728175
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-12-13 23:41:07 -0600

    Removed extraneous lines.

--- include/components/system_components.h
+++ include/components/system_components.h
@@ -6,10 +6,6 @@
 #include "shape.h"
 #include <nlohmann/json.hpp>
 #include "config.h"
-#include <nlohmann/json.hpp>
-#include "config.h"
-#include <nlohmann/json.hpp>
-#include "config.h"
 
 using json = nlohmann::ordered_json;
 

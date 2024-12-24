a372ce8fe5f2f582a53501173484e619a5ebfc06
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-12-23 18:51:03 -0600

    Added a validity check function (currently checks for nans).

--- tests/test_scenario.cpp
+++ tests/test_scenario.cpp
@@ -12,11 +12,25 @@ using namespace twirl;
 
 float MAX_TIME = 0.5f;
 
+void standardValidityChecks(entt::registry& registry) {
+    auto rview = registry.view<PhysicsComp>();
+    int n_nan = 0;
+    for (auto [entity, phys_c] : rview.each()) {
+        if (std::isnan(phys_c.pos.x) || std::isnan(phys_c.pos.y)) {
+            std::cerr << "Warning: Entity " << static_cast<int>(entity)
+                        << " has NaN position.\n";
+            n_nan++;
+        }
+    }
+    ASSERT_EQ(n_nan, 0);
+}
+
 TEST(ScenarioTest, TestScene) {
     // Initialize the game, hidden in the constructor
     Game game("../../tests/test_data/test_scenes/main_test_scene.json");
     game.max_time = MAX_TIME;
     game.run();
+    standardValidityChecks(game.getRegistry());
 }
 
 TEST(ScenarioTest, TestMenuScene) {

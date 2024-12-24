92c9ff452cf4527cbdf9466d669e5543fa9210cb
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-12-23 19:00:01 -0600

    Made standardValidity checks more explicit.

--- tests/test_scenario.cpp
+++ tests/test_scenario.cpp
@@ -10,15 +10,23 @@
 
 using namespace twirl;
 
-float MAX_TIME = 0.5f;
+float MAX_TIME = 1.5f;
 
 void standardValidityChecks(entt::registry& registry) {
+    comp::debugEntities(registry, "Debugging inside standardValidityChecks");
     auto rview = registry.view<PhysicsComp>();
     int n_nan = 0;
     for (auto [entity, phys_c] : rview.each()) {
         if (std::isnan(phys_c.pos.x) || std::isnan(phys_c.pos.y)) {
+
+            auto entity_name_ptr = registry.try_get<EntityName>(entity);
+            if (entity_name_ptr != nullptr) {
+                std::cerr << "Warning: Entity " << *entity_name_ptr
+                          << " has NaN position.\n";
+            } else {    
             std::cerr << "Warning: Entity " << static_cast<int>(entity)
                         << " has NaN position.\n";
+            }
             n_nan++;
         }
     }
